207dd7c72e63a2e3478348beae3096895930a636
richardW8k <richard@wawrzyniak.me>
2014-02-07 16:50:27 +0000

    version and hook changes

--- RWListFieldCalculations.php
+++ RWListFieldCalculations.php
@@ -32,7 +32,7 @@ if ( class_exists( 'GFForms' ) ) {
 
     class RWListFieldCalculations extends GFAddOn {
 
-        protected $_version = '0.1';
+        protected $_version = '0.2';
         protected $_min_gravityforms_version = '1.8.0';
         protected $_slug = 'RWListFieldCalculations';
         protected $_path = 'RWListFieldCalculations/RWListFieldCalculations.php';
@@ -42,28 +42,73 @@ if ( class_exists( 'GFForms' ) ) {
 
         public function init(){
             parent::init();
+            add_action( 'gform_enqueue_scripts', array( $this, 'list_field_calculations_script' ), 10, 2 );
+            add_filter( 'gform_get_form_filter', array( $this, 'list_field_calculations_add_var' ), 10, 2 );
             add_filter( 'gform_calculation_formula', array( $this, 'list_field_calculations' ), 10, 4 );
-            add_filter( 'gform_pre_render', array( $this, 'list_field_calculations_pre_render' ) );
             add_filter( 'gform_custom_merge_tags', array( $this, 'list_field_calculations_merge_tags' ), 10, 4 ) ;
         }
 
-        public function scripts() {
-            $scripts = array(
-                array( 'handle' => 'LFCalc',
-                      'src'     => $this->get_base_url() . '/js/LFCalc.js',
-                      'version' => $this->_version,
-                      'deps'    => array( 'jquery', 'gform_gravityforms' ),
-                      'enqueue' => array(
-                          array( 'field_types' => 
-                                array( 'list','calculation' )
-                               )
-                      )
-                ),
-
-            );
-
-            return array_merge( parent::scripts(), $scripts );
+        function has_list_field_merge_tag( $form, $quick_check ) {
+        
+            $formula_fields = array();
+    
+            foreach ( $form['fields'] as $field ) {
+                if ( !rgar( $field, 'calculationFormula') ) {
+                    continue;
+                }
+    
+                preg_match_all( '/{[^{]*?:(\d+)\.?(\d+)?}/mi', $field['calculationFormula'], $matches, PREG_SET_ORDER );
+    
+                if ( is_array( $matches ) ) {
+    
+                    foreach( $matches as $match ) {
+    
+                        // get the $field object for the provided id
+                        $field_id = $match[1];
+                        $lfield = RGFormsModel::get_field( $form, $field_id );
+    
+                        // check the field type as we only want the rest of the function to run if the field type is list
+                        if ( RGFormsModel::get_input_type( $lfield ) != 'list' ) {
+                            continue;
+                        }
+                        
+                        if ( $quick_check ){
+                            return true;
+                        }
+                        
+                        $columnNo = isset( $match[2] ) ? $match[2] : null;
+                        $formula_fields[] = array( 'mergeTag' => $match[0], 'fieldId' => $match[1], 'columnNo' => $columnNo );
+    
+                    }
+    
+                }
+    
+            }
+            
+            return empty( $formula_fields ) ? false : $formula_fields;
         }
+        
+        function list_field_calculations_script( $form ) {
+            
+            $formula_fields = self::has_list_field_merge_tag( $form, true );
+
+            if ( $formula_fields ) {
+                wp_enqueue_script( 'LFCalc', $this->get_base_url() . '/js/LFCalc.js', array( 'jquery','gform_gravityforms' ), $this->_version, true );
+            }
+   
+        }
+        
+        function list_field_calculations_add_var( $form_string, $form ) {
+
+            $formula_fields = self::has_list_field_merge_tag( $form, false );
+
+            if ( $formula_fields ) {
+                $script = 'var LFCalc = ' . json_encode( $formula_fields ) . ';';
+                $form_string .= "<script type='text/javascript'>{$script}</script>";
+            }
+    
+            return $form_string;
+        }        
         
         function list_field_calculations( $formula, $field, $form, $lead ) {
     
@@ -73,7 +118,7 @@ if ( class_exists( 'GFForms' ) ) {
     
             if( is_array( $matches ) ) {
     
-                foreach( $matches as $match ) {
+                foreach ( $matches as $match ) {
     
                     // $match[0] = merge tag e.g. {List:1.3}
                     // $match[1] = field id e.g. 1
@@ -84,7 +129,7 @@ if ( class_exists( 'GFForms' ) ) {
                     $field = RGFormsModel::get_field( $form, $field_id );
     
                     // check the field type as we only want the rest of the function to run if the field type is list
-                    if( RGFormsModel::get_input_type( $field ) != 'list' ) {
+                    if ( RGFormsModel::get_input_type( $field ) != 'list' ) {
                         continue;
                     }
     
@@ -93,20 +138,20 @@ if ( class_exists( 'GFForms' ) ) {
                     $count = 0;
     
                     // if column number found sum column values otherwise count number of rows
-                    if( isset( $match[2] ) ) {
+                    if ( isset( $match[2] ) ) {
     
                         // count the actual number of columns
                         $column_count = count( rgar( $field,'choices' ) );
     
-                        if( $column_count > 1 ) {
+                        if ( $column_count > 1 ) {
                             // subtract 1 from column number as the choices array is zero based
                             $column_num = $match[2] - 1;
                             // get the column label so we can use that as the key to the multi-column values
                             $column = rgars( $field, "choices/{$column_num}/text" );
                         }
     
-                        foreach( $list_values as $value ) {
-                            if( $column_count == 1 ) {
+                        foreach ( $list_values as $value ) {
+                            if ( $column_count == 1 ) {
                                 $count += GFCommon::to_number( $value );
                             } else {
                                 $count += GFCommon::to_number( $value[$column] );
@@ -126,51 +171,9 @@ if ( class_exists( 'GFForms' ) ) {
     
             // return the modified formula so the rest of the calculation can be run by GF
             return $formula;
-    
-        }
-
-        function list_field_calculations_pre_render( $form ) {
-    
-            $formula_fields = array();
-    
-            foreach ( $form['fields'] as $field) {
-                if( !rgar( $field, 'calculationFormula') ) {
-                    continue;
-                }
-    
-                preg_match_all( '/{[^{]*?:(\d+)\.?(\d+)?}/mi', $field['calculationFormula'], $matches, PREG_SET_ORDER );
-    
-                if( is_array( $matches ) ) {
-    
-                    foreach( $matches as $match ) {
-    
-                        // get the $field object for the provided id
-                        $field_id = $match[1];
-                        $lfield = RGFormsModel::get_field( $form, $field_id );
-    
-                        // check the field type as we only want the rest of the function to run if the field type is list
-                        if( RGFormsModel::get_input_type( $lfield ) != 'list' ) {
-                            continue;
-                        }
-                        
-                        $columnNo = isset( $match[2] ) ? $match[2] : null;
-                        $formula_fields[] = array( 'mergeTag' => $match[0], 'fieldId' => $match[1], 'columnNo' => $columnNo );
-    
-                    }
-    
-                }
-    
-            }
-    
-            if ( ! empty($formula_fields) ) {
-                $script = 'var LFCalc = ' . json_encode( $formula_fields ) . ';';
-                echo "<script>{$script}</script>";
-            }
-    
-            return $form;
         }
         
-        function list_field_calculations_merge_tags($merge_tags, $form_id, $fields, $element_id) {
+        function list_field_calculations_merge_tags( $merge_tags, $form_id, $fields, $element_id ) {
             
             // check the type of merge tag dropdown
             if ( $element_id != 'field_calculation_formula' ) {
@@ -180,7 +183,7 @@ if ( class_exists( 'GFForms' ) ) {
             foreach ( $fields as $field ) {
                 
                 // check the field type as we only want to generate merge tags for list fields       
-                if( $field['type'] != 'list' ) {
+                if ( $field['type'] != 'list' ) {
                     continue;
                 }
                 
@@ -188,7 +191,7 @@ if ( class_exists( 'GFForms' ) ) {
                 $tag = '{' . $label . ':' . $field['id'];
                 $column_count = count( $field['choices'] );
     
-                if( $column_count > 1 ) {
+                if ( $column_count > 1 ) {
                     
                     $i = 0;
                     
