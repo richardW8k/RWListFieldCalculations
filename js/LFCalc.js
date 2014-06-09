10633a5eb5862540cf0b276e00069037d8ec7d51
richardW8k <richardW8k@users.noreply.github.com>
2014-06-09 15:00:28 +0100

    Update LFCalc.js
    Removed redundant inputId var.

--- js/LFCalc.js
+++ js/LFCalc.js
@@ -1,32 +1,30 @@
 gform.addAction( 'gform_calculation_events', function( mergeTag, formulaField, formId, calcObj ){
-    var inputId = mergeTag[1],
-        fieldId = parseInt( inputId ),
+    var fieldId = parseInt( mergeTag[1] ),
         fieldSelector = '#field_' + formId + '_' + fieldId;
 
     if ( jQuery( fieldSelector + ' table.gfield_list' ).length == 1 ) {
         jQuery( fieldSelector )
             .on( 'click', '.add_list_item', function () {
                 jQuery( fieldSelector + ' .delete_list_item' ).removeProp( 'onclick' );
-                calcObj.bindCalcEvent( inputId, formulaField, formId, 0 );
+                calcObj.bindCalcEvent( fieldId, formulaField, formId, 0 );
             })
             .on( 'click', '.delete_list_item', function () {
                 gformDeleteListItem( this, 0 );
-                calcObj.bindCalcEvent( inputId, formulaField, formId, 0 );
+                calcObj.bindCalcEvent( fieldId, formulaField, formId, 0 );
             });
 
         if ( mergeTag[2] != null ) {
             var columnNo = mergeTag[2].substr( 1 ),
                 columnSelector = '.gfield_list_' + fieldId + '_cell' + columnNo + ' :input';
             jQuery( fieldSelector ).on( 'change', columnSelector, function () {
-                calcObj.bindCalcEvent( inputId, formulaField, formId, 0 );
+                calcObj.bindCalcEvent( fieldId, formulaField, formId, 0 );
             });
         }
     }
 });
 
 gform.addFilter( 'gform_calculation_merge_tag_value', function( value, mergeTag, isVisible, formulaField, formId ){
-    var inputId = mergeTag[1],
-        fieldId = parseInt( inputId ),
+    var fieldId = parseInt( mergeTag[1] ),
         fieldSelector = '#field_' + formId + '_' + fieldId,
         cellValue = 0;
 
